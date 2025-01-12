23954b130d17fead76f807b98fca0356baff88ce
loidinhm31 <loidinh.git@gmail.com>
2025-01-12 17:01:50 +0700

    Refactor

---
+++ demo-be/src/main/java/com/example/demo2/exceptions/OAuth2AuthenticationProcessingException.java
@@ -0,0 +1,14 @@
+package com.example.demo2.exceptions;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+    public OAuth2AuthenticationProcessingException(String msg) {
+        super(msg);
+    }
+
+    public OAuth2AuthenticationProcessingException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
+
