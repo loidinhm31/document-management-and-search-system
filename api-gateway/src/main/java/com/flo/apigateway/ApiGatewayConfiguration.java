a97c455dabe9e96ca1d307ca26a9ee9872647a4d
loidinhm31 <loidinh.git@gmail.com>
2025-01-18 14:03:04 +0700

    Add API gateway

---
+++ api-gateway/src/main/java/com/flo/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,26 @@
+package com.flo.apigateway;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r-> r.path("/auth/**")
+                        .filters(f -> f.rewritePath(
+                                "/auth/(?<segment>.*)",
+                                "/${segment}"))
+                        .uri("lb://auth-service"))
+                .route(r-> r.path("/document/**")
+                        .filters(f -> f.rewritePath(
+                                "/document/(?<segment>.*)",
+                                "/${segment}"))
+                        .uri("lb://document-service"))
+                .build();
+    }
+}
