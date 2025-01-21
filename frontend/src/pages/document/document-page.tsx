9bd0d0f5d412c0a0086cfd9fee41d221926276b0
loidinhm31 <loidinh.git@gmail.com>
2025-01-22 01:34:04 +0700

    Fix document list layout

--- frontend/src/pages/document/document-page.tsx
+++ frontend/src/pages/document/document-page.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
+import React from "react";
 
 import { DocumentList } from "@/components/document/document-list";
 import { DocumentUpload } from "@/components/document/document-upload";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function DocumentsPage() {
   return (
     <div className="space-y-6">
-      <Card>
-        <CardHeader>
-          <CardTitle>Upload Document</CardTitle>
-          <CardDescription>
-            Drag and drop your document or click to browse
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <DocumentUpload />
-        </CardContent>
-      </Card>
+      <DocumentUpload />
 
       <DocumentList />
     </div>
