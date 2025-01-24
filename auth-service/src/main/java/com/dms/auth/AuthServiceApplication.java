9a1644f73ee6530042c679a78d199eb0da519eb3
loidinhm31 <loidinh.git@gmail.com>
2025-01-25 06:17:50 +0700

    Try to fix config eureka client

--- auth-service/src/main/java/com/dms/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/dms/auth/AuthServiceApplication.java
@@ -2,11 +2,13 @@ package com.dms.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- document-service/src/main/java/com/dms/document/DocumentServiceApplication.java
+++ document-service/src/main/java/com/dms/document/DocumentServiceApplication.java
@@ -2,10 +2,12 @@ package com.dms.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.dms.document.client")
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.dms.document.client")
 public class DocumentServiceApplication {
 
     public static void main(String[] args) {
