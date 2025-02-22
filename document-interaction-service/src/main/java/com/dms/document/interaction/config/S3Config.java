37da231687c27e86d91e47b3617824df4919b990
loidinhm31 <loidinh.git@gmail.com>
2025-02-22 12:03:05 +0700

    S3 config for localstack

--- document-interaction-service/src/main/java/com/dms/document/interaction/config/S3Config.java
+++ document-interaction-service/src/main/java/com/dms/document/interaction/config/S3Config.java
@@ -1,6 +1,7 @@
 package com.dms.document.interaction.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -8,9 +9,14 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.net.URI;
+
 @Configuration
 @RequiredArgsConstructor
 public class S3Config {
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
     private final S3Properties s3Properties;
 
     @Bean
@@ -19,6 +25,15 @@ public class S3Config {
                 s3Properties.getAccessKey(),
                 s3Properties.getSecretKey()
         );
+
+        if ("local".equals(activeProfile)) {
+            return S3Client.builder()
+                    .region(Region.of(s3Properties.getRegion()))
+                    .endpointOverride(URI.create("http://localhost:4566")) // LocalStack endpoint
+                    .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                    .forcePathStyle(true)
+                    .build();
+        }
 
         return S3Client.builder()
                 .region(Region.of(s3Properties.getRegion()))
